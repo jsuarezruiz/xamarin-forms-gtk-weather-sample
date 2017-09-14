f6a10e30aff2e0afb0cf4450781686baff93126c
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2017-09-14 13:15:20 +0200

    Added more platforms

---
+++ src/WeatherApp/WeatherApp.UWP/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WeatherApp.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new WeatherApp.App());
+        }
+    }
+}
